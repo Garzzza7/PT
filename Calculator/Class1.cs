fab6f15a97a1d79c4fbbb4274eb4df3fa110f3eb
Piotr Czapla <piotr.czapela@gmail.com>
2022-03-20 21:37:34 +0100

    First Commit comment

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -2,6 +2,7 @@
 {
     public class Class1
     {
+
         public int add(int a, int b) { return a + b; }
         public int substract(int a, int b) { return a - b; }
         public int multiply(int a, int b) { return a * b; }
