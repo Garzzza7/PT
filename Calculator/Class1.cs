a4fd9b479528b74f8a1b228902e76c3809ce9c78
Piotr Czapla <piotr.czapela@gmail.com>
2022-03-21 17:40:22 +0100

    a very nice commit

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -2,7 +2,7 @@
 {
     public class Class1
     {
-
+        //a very nice comment 
         public int add(int a, int b) { return a + b; }
         public int substract(int a, int b) { return a - b; }
         public int multiply(int a, int b) { return a * b; }
