14c15e71755fbe488f44035cfab6a64de6498daa
Piotr Czapla <234751@edu.p.lodz.pl>
2022-05-09 22:32:11 +0200

    logic tests modifications

--- Task1/ShopSystemTest/Tests/LogicLayerTest.cs
+++ Task1/ShopSystemTest/Tests/LogicLayerTest.cs
@@ -12,6 +12,7 @@ namespace ShopSystemTest
     public class LogicLayerTest
     {
 
+        
         private LogicLayerAPI logic;
         private Service service;
 
@@ -20,62 +21,68 @@ namespace ShopSystemTest
         {
             logic = LogicLayerAPI.CreateLayer();
             service = new Service(logic);
+           
         }
 
         [TestMethod]
         public void Addclienttest()
         {
             
+            service.AddClient(2, "Jamie", "Olivier");
+            Assert.AreEqual(service.GetClient(2).Name, "Jamie");
+            Assert.AreEqual(service.GetAllClients().Count(), 1);
         }
 
         [TestMethod]
         public void RemoveClienttest()
         {
-
+            Assert.AreEqual(service.GetAllClients().Count(), 0);
+            service.AddClient(2, "Jamie", "Olivier");
+           
         }
-        /*
+
         [TestMethod]
-        public void test()
+        public void test1()
         {
 
         }
 
         [TestMethod]
-        public void test()
+        public void test2()
         {
 
         }
 
         [TestMethod]
-        public void test()
+        public void test3()
         {
 
         }
 
         [TestMethod]
-        public void test()
+        public void test4()
         {
 
         }
 
         [TestMethod]
-        public void test()
+        public void test5()
         {
 
         }
 
         [TestMethod]
-        public void test()
+        public void test6()
         {
 
         }
 
         [TestMethod]
-        public void test()
+        public void test7()
         {
 
         }
-        */
+
 
     }
 }
