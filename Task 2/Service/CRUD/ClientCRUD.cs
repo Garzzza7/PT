63b4977eabb93d0cfc9924e0750cb07064027719
Piotr Czapla <234751@edu.p.lodz.pl>
2022-05-21 19:07:59 +0200

    LOL

--- Task 2/Service/CRUD/ClientCRUD.cs
+++ Task 2/Service/CRUD/ClientCRUD.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.DataBase;
 
 namespace Service
 {
@@ -15,7 +16,7 @@ namespace Service
         static public bool AddClient(int ID , string name , string surname)
         {
             using (DataClasses1DataContext context = new DataClasses1DataContext())
-            {
+            {//lolo
                 Clients__ newCustomer = new Clients__
                 {
                     customer_id = id,
